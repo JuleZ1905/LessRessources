fed4fca555abc65957ae0dafbbdc24c2dc3d680f
Hannah <8068@htl.rennweg.at>
2021-12-21 12:26:30 +0100

    login

---
+++ src/Website/Sites/login.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" a href="../../css/login.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div class="container">
+            <img src="../../lib/logo.jpg"/>
+            <form>
+                <div class="form-input">
+                    <input type="text" name="text" placeholder="Enter your username"/>
+                </div>
+                <div class="form-input">
+                    <input type="password" name="password" placeholder="Enter your password"/>
+                </div>
+                <input type="submit" type="submit" value="LOGIN" class="btn-login"/>
+            </form>
+        </div>
+    </body>
+</html>
