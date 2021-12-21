3329c16533028ab62507859af22ac780c06ed59a
Hannah <8068@htl.rennweg.at>
2021-12-21 13:25:22 +0100

    popup fenster versuch 1

--- src/Website/Sites/login.php
+++ src/Website/Sites/login.php
@@ -1,4 +1,7 @@
 <?php
+if (isset($POST['username'])){
+    //TODO Daten überprüfen
+}
 ?>
 
 <!DOCTYPE html>
@@ -7,12 +10,15 @@
         <title>Login</title>
         <link rel="stylesheet" a href="../../css/login.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../../js/dialoge.js">
+        <link rel="stylesheet" href="../../css/dialoge.css">
         <meta charset="UTF-8">
     </head>
     <body>
+    <main>
         <div class="container">
             <img src="../../lib/logo.jpg"/>
-            <form>
+            <form method="post" action="#">
                 <div class="form-input">
                     <input type="text" name="text" placeholder="Enter your username"/>
                 </div>
@@ -22,5 +28,22 @@
                 <input type="submit" type="submit" value="LOGIN" class="btn-login"/>
             </form>
         </div>
+    </main>
+
+    <div class="dialog" id="loslegen-dialog">
+        <a href="#" role="button" class="dialog-schliessen-button" onclick="dialogSchliessen('loslegen-dialog')">
+            <p>test</p>
+        </a>
+        <h3>Jetzt Registrieren!</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, ipsa ad doloribus perferendis debitis dignissimos facilis beatae architecto aspernatur quae tempora repellat nihil fugiat quod!</p>
+        <form action="">
+            <input type="text" placeholder="Vorname" class="eingabefeld">
+            <input type="text" placeholder="Nachname" class="eingabefeld">
+            <input type="email" placeholder="Email-Adresse" class="eingabefeld">
+            <input type="password" placeholder="Passwort" class="eingabefeld">
+            <input type="submit" value="Loslegen!">
+        </form>
+    </div>
+    <script src="../../js/dialoge.js"></script>
     </body>
 </html>
