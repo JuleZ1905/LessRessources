dc5ae14e859ea82571063170e94fd2497f39f633
Hannah <8068@htl.rennweg.at>
2021-12-31 13:12:18 +0100

    popup fenster fertig datenüberprüfung fehlt

--- src/Website/Sites/login.php
+++ src/Website/Sites/login.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($POST['username'])){
+if (isset($POST['username'])) {
     //TODO Daten überprüfen
 }
 ?>
@@ -10,40 +10,34 @@ if (isset($POST['username'])){
         <title>Login</title>
         <link rel="stylesheet" a href="../../css/login.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="../../js/dialoge.js">
-        <link rel="stylesheet" href="../../css/dialoge.css">
         <meta charset="UTF-8">
     </head>
-    <body>
-    <main>
-        <div class="container">
-            <img src="../../lib/logo.jpg"/>
-            <form method="post" action="#">
-                <div class="form-input">
-                    <input type="text" name="text" placeholder="Enter your username"/>
-                </div>
-                <div class="form-input">
-                    <input type="password" name="password" placeholder="Enter your password"/>
-                </div>
-                <input type="submit" type="submit" value="LOGIN" class="btn-login"/>
-            </form>
-        </div>
-    </main>
 
-    <div class="dialog" id="loslegen-dialog">
-        <a href="#" role="button" class="dialog-schliessen-button" onclick="dialogSchliessen('loslegen-dialog')">
-            <p>test</p>
-        </a>
-        <h3>Jetzt Registrieren!</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, ipsa ad doloribus perferendis debitis dignissimos facilis beatae architecto aspernatur quae tempora repellat nihil fugiat quod!</p>
-        <form action="">
-            <input type="text" placeholder="Vorname" class="eingabefeld">
-            <input type="text" placeholder="Nachname" class="eingabefeld">
-            <input type="email" placeholder="Email-Adresse" class="eingabefeld">
-            <input type="password" placeholder="Passwort" class="eingabefeld">
-            <input type="submit" value="Loslegen!">
+    <body>
+    <button class="btn-login" onclick="openForm()">Login</button>
+
+    <div class="container form-popup" id="myForm">
+        <img src="../../lib/logo.jpg"/>
+        <form method="post" action="#">
+            <div class="form-input">
+                <input type="text" name="text" placeholder="Enter your username"/>
+            </div>
+            <div class="form-input">
+                <input type="password" name="password" placeholder="Enter your password"/>
+            </div>
+            <input type="submit" type="submit" value="LOGIN" class="btn-login"/>
+            <input type="button" value="CLOSE" class="btn-login" onclick="closeForm()"/>
         </form>
     </div>
-    <script src="../../js/dialoge.js"></script>
     </body>
+
+    <script>
+        function openForm() {
+            document.getElementById("myForm").style.display = "block";
+        }
+
+        function closeForm() {
+            document.getElementById("myForm").style.display = "none";
+        }
+    </script>
 </html>
--- src/js/dialoge.js
+++
@@ -1,9 +0,0 @@
-function dialogOeffnen(dialogId) {
-    document.getElementById(dialogId).classList.add("sichtbar");
-    document.getElementById("body-overlay").classList.add("sichtbar");
-}
-
-function dialogSchliessen(dialogId) {
-    document.getElementById(dialogId).classList.remove("sichtbar");
-    document.getElementById("body-overlay").classList.remove("sichtbar");
-}
