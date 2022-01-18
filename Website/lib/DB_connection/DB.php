3125f4451b208bbc791b73f86798439343fc7451
JuleZ_2k4 <8096@htl.rennweg.at>
2022-01-18 09:38:25 +0100

    DB Name changed

--- Website/lib/DB_connection/DB.php
+++ Website/lib/DB_connection/DB.php
@@ -1,4 +1,4 @@
 <?php
-$db = new PDO("mysql:host=localhost; dbname=LessRessources; charset=utf8", "root");
+$db = new PDO("mysql:host=sql200.epizy.com; dbname=epiz_30590835_LessRessources; charset=utf8", "epiz_30590835", "LessRess2021!");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 ?>
